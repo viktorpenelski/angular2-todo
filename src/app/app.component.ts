f8784339c84832d1dcc42bd63e5bd3f59bfb673a
Viktor Penelski <viktor.penelski@paysafe.com>
2016-10-21 16:49:01 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { TodoAppComponent } from './todo-app/todo-app.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [TodoAppComponent]
 })
 export class AppComponent {
   title = 'app works!';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { TodoAppComponent } from './app/todo-app';
 
 if (environment.production) {
   enableProdMode();
